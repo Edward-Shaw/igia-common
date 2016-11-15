df06e1e320432a7c8a61b58c4ff811325760bdf7
Edward-Shaw <yxiao@aliyun.com>
2016-11-15 16:48:50 +0800

    add content.

--- src/main/java/com/cloume/shaw/igia/common/resource/Course.java
+++ src/main/java/com/cloume/shaw/igia/common/resource/Course.java
@@ -46,7 +46,20 @@ public class Course {
 	 * 课程状态
 	 */
 	private String state;
+	
+	/**
+	 * 课程内容
+	 */
+	private String content;
 
+	public String getId(){
+		return id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
 	public String getCode() {
 		return code;
 	}
@@ -101,5 +114,13 @@ public class Course {
 
 	public void setState(String state) {
 		this.state = state;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
 	}
 }
